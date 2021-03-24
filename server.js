a54f1fef2fe48f234a55172a77277f81028417c0
yajatvishwak <yajat472001@gmail.com>
2021-03-24 23:35:12 +0530

    remove hostname

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const http = require("http");
 const app = require("./app");
-const hostname = "0.0.0.0";
 
 const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-server.listen(port, hostname, (err) => {
+server.listen(port, (err) => {
   if (err) console.error(err);
   console.log("Listening on port:", port);
 });
