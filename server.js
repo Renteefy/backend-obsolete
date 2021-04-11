180ea2bd19ab709d3ad2c839c89e9ea7b85bddef
yajatvishwak <yajat472001@gmail.com>
2021-04-11 13:01:57 +0530

    added https to local host for easier dev

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const http = require("http");
+const https = require("https");
 const app = require("./app");
 
 const port = process.env.PORT || 5000;
 
-const server = http.createServer(app);
+//const server = http.createServer(app);
 
-server.listen(port, (err) => {
-  if (err) console.error(err);
-  console.log("Listening on port:", port);
-});
+const httpsLocalhost = require("https-localhost")();
+
+async function resolveCertificates() {
+  const certs = await httpsLocalhost.getCerts();
+  console.log(certs);
+  const server = https.createServer(certs, app);
+
+  server.listen(port, (err) => {
+    if (err) console.error(err);
+    console.log("Listening on port:", port);
+  });
+}
+
+resolveCertificates();
