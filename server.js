06a9f40e1980a533ab4112ce8d5b21714d28a6b1
yajatvishwak <yajat472001@gmail.com>
2021-04-12 23:16:58 +0530

    added popdb

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const app = require("./app");
 
 const port = process.env.PORT || 5000;
 
-const server = http.createServer(app);
-
 //const httpsLocalhost = require("https-localhost")();
 
 async function resolveCertificates() {
